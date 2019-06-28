53238cf42b6cf2d47a366d09ebeef30d3f1f784c
Mehul Raj Kumawat <mehul.kumawat1804@gmail.com>
2019-06-28 14:58:11 +0530

    Added custom user profile model, manager and migration again

--- backend_api/models.py
+++ backend_api/models.py
@@ -14,7 +14,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError("User must have an email address.")
 
-        email = sel.normalize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
